f800d13890915356811fc3444470ca52850d35e2
Raahim Irfan <94684550+raahimirfan100@users.noreply.github.com>
2026-01-23 22:14:25 +0500

    Remove committed generated client - let Tina generate it during build

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/raahimirfan/Documents/GitHub/acmnuceskhi/tech-operations-showcase/tina/__generated__/.cache/1769184563314', url: 'https://content.tinajs.io/2.0/content/8916f5c0-d6f7-4dbb-aa24-24c9824e28fb/github/main', token: '5ea70292f12fc1e47fff6e3df178d9ad274e8ef3', queries,  });
-export default client;
-  
